75a3e3d47a4a148147e8bdcd76d55ea940de6135
HisashiHasebe <hhasebe@diverta.co.jp>
2023-10-12 13:09:34 +0800

    update:ニュース一覧ページ

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>実装済み機能一覧</h1>
+        <ul>
+            <li><nuxt-link to="/news/">ニュース一覧</nuxt-link></li>
+        </ul>
+    </div>
+</template>
+  
+  
---
+++ pages/news/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div v-for="n in count.list" :key="n.topics_id">
+      <nuxt-link :to="`/news/${n.topics_id}/`">{{ n.ymd }} {{ n.subject }}</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data: count } = await useFetch('https://hasebetest.g.kuroco.app/rcms-api/1/news')
+</script>
