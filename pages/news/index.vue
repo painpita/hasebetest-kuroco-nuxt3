ca988c57a4d9ac9abc076f8320cd4a616115634d
Jaswant Singh <jaswant_s@ch.iitr.ac.in>
2024-02-06 14:45:15 +0900

    add details

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
-    <div v-for="n in count.list" :key="n.topics_id">
-      <nuxt-link :to="`/news/${n.topics_id}/`">{{ n.ymd }} {{ n.subject }}</nuxt-link>
+    <p>News list</p>
+    <div v-for="n in response.list" :key="n.topics_id">
+      <nuxt-link :to="`/news/${n.topics_id}`">
+        {{ n.ymd }} {{ n.subject }}
+      </nuxt-link>
     </div>
   </div>
 </template>
 
 <script setup>
-const config = useRuntimeConfig()
-const { data: count } = await useFetch('/rcms-api/1/news',{
-  baseURL:config.public.apiBase
-})
+const config = useRuntimeConfig();
+const { data: response } = await useFetch('/rcms-api/1/news',{
+  baseURL:config.public.apiBase,
+  credentials: 'include',
+});
 </script>
