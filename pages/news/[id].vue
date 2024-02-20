2d0b2f750f077b5483da933932df37e02b63559c
HisashiHasebe <hhasebe@diverta.co.jp>
2024-02-20 17:24:31 +0800

    fix:miss url

--- pages/news/[id].vue
+++ pages/news/[id].vue
@@ -10,7 +10,7 @@ const route = useRoute();
 const config = useRuntimeConfig();
 
 const { data: response } = await useFetch(
-  `${config.public.apiBase}/rcms-api/1/news/details/${route.params.id}`,
+  `${config.public.apiBase}/rcms-api/1/newsdetail/${route.params.id}`,
   {
     credentials: 'include',
   }, route.params.id
