f00611e6dcc2bef9e718db2b691de8672c587c90
HisashiHasebe <hhasebe@diverta.co.jp>
2024-02-21 12:37:12 +0800

    add:clientonly

--- pages/news/preview.vue
+++ pages/news/preview.vue
@@ -1,20 +1,22 @@
 <template>
+  <ClientOnly>
     <div>
       <h1 class="title">{{ response.details.subject }}</h1>
       <div class="post" v-html="response.details.contents"></div>
     </div>
-  </template>
+  </ClientOnly>
+</template>
   
-  <script setup>
-  const route = useRoute();
-  
-  const {data: response} = useFetch(`/rcms-api/1/news/preview`, {
-    baseURL:config.public.apiBase,
-    credentials: 'include',
-    server: false,
-    params: {
-      preview_token: route.query.preview_token,
-    },
-  });
-  
-  </script>
+<script setup>
+const route = useRoute();
+
+const { data: response } = useFetch(`/rcms-api/1/news/preview`, {
+  baseURL: config.public.apiBase,
+  credentials: 'include',
+  server: false,
+  params: {
+    preview_token: route.query.preview_token,
+  },
+});
+
+</script>
