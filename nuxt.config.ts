09e497d8bb7172f06265e012e297c59d5fbf2000
HisashiHasebe <hhasebe@diverta.co.jp>
2024-03-08 15:42:55 +0800

    ssr:false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    ssr: false,
     runtimeConfig: {
         // Public keys that are exposed to the client
         public: {
