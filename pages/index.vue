6ce420bc03fc6ad64d3925c3aa695ccfa60f524c
HisashiHasebe <hhasebe@diverta.co.jp>
2024-04-03 15:46:01 +0800

    Revert "test"
    This reverts commit 72507b87e381f533570eeaa076780d582d58e7ad.

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,6 @@
         <ul>
             <li><nuxt-link to="/news/">ニュース一覧</nuxt-link></li>
             <li><nuxt-link to="/form/form_validate">フォームバリデーション</nuxt-link></li>
-            <li>test</li>
         </ul>
     </div>
 </template>
