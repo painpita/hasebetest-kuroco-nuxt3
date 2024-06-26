741a921bf14d73ad5af4a4c924ba9e3a96b40a36
HisashiHasebe <hhasebe@diverta.co.jp>
2024-06-26 10:11:49 +0800

    ページネーション追加

---
+++ components/Pagenator.vue
@@ -0,0 +1,62 @@
+<template>
+    <ul>
+        <li>
+            <nuxt-link :to="{
+                path: $route.path,
+                query: { page: pageNo - 1 },
+            }" @click.native="refresh(pageNo - 1)" :class="{ disabled: ($route.query.page || 1) <= 1 }">
+                Previous
+            </nuxt-link>
+        </li>
+
+        <li v-for="i in totalPageCnt" :key="i">
+            <nuxt-link :to="{ path: $route.path, query: { page: i } }" @click.native="refresh(i)" :class="{
+                disabled: pageNo === i,
+            }">
+                {{ i }}
+            </nuxt-link>
+        </li>
+        <li v-if="pageNo === totalPageCnt">Next</li>
+        <li v-else>
+            <nuxt-link :to="{
+                path: $route.path,
+                query: { page: pageNo + 1 },
+            }" :class="{
+                disabled: totalPageCnt === $route.query.page,
+            }" @click.native="refresh(pageNo + 1)">
+                Next
+            </nuxt-link>
+        </li>
+    </ul>
+</template>
+<script setup>
+const props = defineProps({
+    pageNo: {
+        required: true,
+        type: Number,
+    },
+    totalPageCnt: {
+        required: true,
+        type: Number,
+    },
+});
+
+const emit = defineEmits(["pageUpdate"]);
+
+const refresh = (i) => {
+    emit("pageUpdate", i);
+};
+</script>
+<style scoped>
+ul {
+    list-style: none;
+    display: flex;
+}
+
+.disabled {
+    pointer-events: none;
+    cursor: default;
+    text-decoration: none;
+    color: black;
+}
+</style>
---
+++ pages/news_paginated/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>News list page{{ this.$route.query.page }}</p>
+    <div v-for="n in response.list" :key="n.slug">
+      <nuxt-link :to="`/news_paginated/${n.topics_id}`">
+        {{ n.ymd }} {{ n.subject }}
+      </nuxt-link>
+    </div>
+    <Pagenator v-bind="{ ...response.pageInfo }" @page-update="updatePage" />
+  </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig();
+const route = useRoute();
+const currentPage = ref(route.query.page || 1);
+
+const response = ref(null);
+response.value = await fetchNews(route.query.page || 1);
+
+async function updatePage(page) {
+  currentPage.value = page;
+  response.value = await fetchNews(currentPage.value);
+}
+
+async function fetchNews(page) {
+  const res = await useFetch(`/rcms-api/1/news?pageID=${page}`, {
+    baseURL: config.public.apiBase,
+    credentials: "include",
+  }).then((res) => res.data.value);
+  return res;
+}
+</script>
