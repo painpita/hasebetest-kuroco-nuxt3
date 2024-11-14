3a0f55dd55aa5dfb3671763fabdc97ac68b92c0b
painpita <dimitri@diverta.co.jp>
2024-11-14 21:37:03 +0900

    nuxt3 login page

---
+++ pages/login/test_login4.vue
@@ -0,0 +1,56 @@
+<template>
+  <form @submit.prevent="login">
+    <p v-if="loginStatus !== null" :style="{ color: resultMessageColor }">
+      {{ resultMessage }}
+    </p>
+    <input v-model="email" name="email" type="email" placeholder="email" />
+    <input
+      v-model="password"
+      name="password"
+      type="password"
+      placeholder="password"
+    />
+    <button type="submit">Login</button>
+    <div>
+      <nuxt-link to="/news"> news list </nuxt-link>
+    </div>
+  </form>
+</template>
+
+<script setup>
+import { useStore } from "~/stores/index";
+const store = useStore();
+const config = useRuntimeConfig();
+
+const email = ref("");
+const password = ref("");
+
+const loginStatus = ref(null);
+const resultMessage = ref(null);
+let resultMessageColor = computed(() => {
+  switch (loginStatus.value) {
+    case "success":
+      return "green";
+    case "failure":
+      return "red";
+    default:
+      return "";
+  }
+});
+
+async function login() {
+  try {
+    const payload = {
+      email: email.value,
+      password: password.value,
+    };
+    await store.login(payload);
+    loginStatus.value = "success";
+    resultMessage.value = "Login successful";
+  } catch (e) {
+    console.log(e);
+    loginStatus.value = "failure";
+    resultMessage.value = "Login failed";
+  }
+}
+</script>
